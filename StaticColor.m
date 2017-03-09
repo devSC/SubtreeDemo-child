fefa76363cfc556006ad09f3c118fad21d71e73f
Wilson Yuan <yuansc@nihao.com>
2017-03-09 18:00:34 +0800

    add [StaticColor]

---
+++ StaticColor.m
@@ -0,0 +1,17 @@
+//
+//  StaticColor.m
+//  SubtreeDemo
+//
+//  Created by Wilson Yuan on 2017/3/9.
+//  Copyright © 2017年 Wilson Yuan. All rights reserved.
+//
+
+#import "StaticColor.h"
+
+@implementation StaticColor
+
++ (void)hello {
+    NSLog(@"%s", __FUNCTION__);
+}
+
+@end
